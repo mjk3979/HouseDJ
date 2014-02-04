bfe57d9278ff297e24f12df8b66a319d70dc12d5
Matthew Koontz <mjk3979@rit.edu>
2014-02-03 22:43:01 -0500

    Added __str__ to Song.

--- common.py
+++ common.py
@@ -4,3 +4,6 @@ class Song:
     def __init__(self, title, artist):
         self.title = title
         self.artist = artist
+
+    def __str__(self):
+        return self.title + " by " + self.artist
