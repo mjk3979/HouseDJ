548cdf01fc2c79809cd0659b29b79c00540ce494
Matthew Koontz <mjk3979@rit.edu>
2014-02-03 22:10:49 -0500

    Fixed str not bufferable error.

--- client.py
+++ client.py
@@ -7,6 +7,6 @@ BUFFER_SIZE = 20
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 s.connect((SERVER_IP,SERVER_PORT))
-s.send(input())
+s.send(bytes(input(), 'UTF-8'))
 print (s.recv(1024))
 s.close()
