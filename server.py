9d6e45cc75fd843edafa2933970c113c4ea6baf0
Matthew Koontz <mjk3979@rit.edu>
2014-02-03 21:45:40 -0500

    Wrote server side of TCP tutorial.

---
+++ server.py
@@ -0,0 +1,18 @@
+import socket
+
+SERVER_IP = '127.0.0.1'
+SERVER_PORT = 5555
+BUFFER_SIZE = 20
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((SERVER_IP, SERVER_PORT))
+s.listen(1)
+
+conn, addr = s.accept
+print ('Client connected:', addr)
+while 1:
+    data = conn.recv(BUFFER_SIZE)
+    if not data: break
+    print ('got data: ', data)
+    conn.send(data)
+conn.close()
