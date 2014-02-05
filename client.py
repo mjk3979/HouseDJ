09c043ac031c1916bb3b6fbe0138cde79e341601
Joe <jwc2790@rit.edu>
2014-02-04 21:42:50 -0500

    mp3 tags

--- client.py
+++ client.py
@@ -1,8 +1,18 @@
+#!usr/bin/python
 import zmq
 from common import Song
 import pickle
+from mutagenx.easyid3 import EasyID3
+import sys
 
-song = Song("Beast and the Harlot", "A7X")
+
+
+mp3 = EasyID3(sys.argv[1])
+print(type(mp3["title"]))
+print(mp3["title"])
+song = Song(mp3["title"][0],mp3["artist"][0])
+	
+#song = Song("Beast and the Harlot", "A7X")
 
 SERVER_IP= '127.0.0.1'
 SERVER_PORT = 5555
