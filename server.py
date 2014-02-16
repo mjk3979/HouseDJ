d0cec0f40567b27b58a054f2b15afd953859f4bc
Matthew Koontz <mjk3979@rit.edu>
2014-02-16 18:05:34 -0500

    Integrated the new MasterQueue class.

--- server.py
+++ server.py
@@ -8,11 +8,13 @@ import pygame
 from pydub import AudioSegment
 from io import BytesIO
 import sys
+from MasterQueue import *
+from copy import deepcopy
 
 SERVER_IP = '127.0.0.1'
 clients = {}
 currentIndex = 0
-masterQueue = None
+masterQueue = MasterQueue()
 socket = None
 songMap = {}
 queueLock = Lock()
@@ -26,23 +28,14 @@ contextPub = zmq.Context()
 socketPub = contextPub.socket(zmq.PUB)
 socketPub.bind("tcp://*:%s" % (PUBLISH_PORT,))
 
-def incCurrentIndex():
-	global currentIndex
-	currentIndex = (currentIndex + 1) % len(clients)
-
 def playerLoop():
 	global currentIndex, masterQueue, songMap
 	pygame.mixer.init(44100)
 	while True:
-		if masterQueue != None and len(masterQueue) != 0 and masterQueue[0][1] in songMap and not(pygame.mixer.get_busy()):
+		if masterQueue.peek() != None and masterQueue.peek()[1] in songMap and not(pygame.mixer.get_busy()):
 			print("HERE")
-			cli, song = masterQueue.pop(0)
-			cli.lock.acquire()
-			cli.queue.pop(0)
-			cli.lock.release()
-			currentIndex = cli.index
-			incCurrentIndex()
-			updateMasterQueue()
+			cli, song = masterQueue.pop()
+			publishQueue()
 			songdata = songMap[song]
 			print("CONVERTING")
 			aseg = AudioSegment.from_file(BytesIO(songdata))
@@ -61,51 +54,29 @@ def playerLoop():
 def publishQueue():
 	global socketPub, publishLock
 	publishLock.acquire()
-	socketPub.send(pickle.dumps([(cli.clientdata, song) for (cli, song) in masterQueue]))
+	socketPub.send(pickle.dumps(masterQueue.toList()))
 	publishLock.release()
 
-
-def getNewIndex():
-	if len(clients) == 0:
-		return 0
-	return max(c.index for c in clients.values()) + 1
-
-def updateMasterQueue():
-	global masterQueue, currentIndex, clients, queueLock
-	queueLock.acquire()
-	masterQueue = []
-	i = currentIndex
-	songsFromEach = 0
-	orderClients = [c for (i,c) in sorted((cli.index, cli) for cli in clients.values())]
-	while songsFromEach < max(len(c.queue) for c in orderClients):
-		print("HERE")
-		songsFromEach += 1
-		for ci in range(len(orderClients)):
-			ind = (i + ci) % len(orderClients)
-		if len(orderClients[ind].queue) >= songsFromEach:
-			masterQueue.append((orderClients[ind], orderClients[ind].queue[songsFromEach - 1]))
-	publishQueue()
-	queueLock.release()
-
 class Client():
-	__slots__=('clientdata', 'queue', 'index', 'port', 'lock')
+	__slots__=('clientdata', 'queue', 'port', 'lock')
 
 	def __init__(self, clientdata, port):
 		self.clientdata = clientdata
 		self.queue = []
-		self.index = getNewIndex()
 		self.port = port
 		self.lock = Lock()
 
 	def handleMessage(self, message):
-		self.lock.acquire()
 		# Queue update
 		if (type(message) is Song):
+			self.lock.acquire()
 			self.queue.append(message)
-			updateMasterQueue()
+			self.lock.release()
+			publishQueue()
 		else:
+			self.lock.acquire()
 			songMap[message[0]] = message[1]
-		self.lock.release()
+			self.lock.release()
 
 	def run(self):
 		context = zmq.Context()
@@ -114,6 +85,11 @@ class Client():
 		while True:
 			data = pickle.loads(mysock.recv())
 			self.handleMessage(data)
+
+	def __deepcopy__(self, memo):
+		copy = Client(deepcopy(self.clientdata, memo), self.port)
+		copy.queue = [deepcopy(s, memo) for s in self.queue]
+		return copy
 
 def listenNewPort():
 	context = zmq.Context()
@@ -130,6 +106,7 @@ def clientLoop():
 		if not clientdata in clients:
 			newClient = Client(clientdata,listenNewPort())
 			clients[clientdata] = newClient
+			masterQueue.addClient(newClient)
 			Thread(target=clients[clientdata].run).start()
 		socket.send(pickle.dumps((clients[clientdata].port,PUBLISH_PORT)))
 
